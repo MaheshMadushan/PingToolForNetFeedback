f845942f0b3ce4016629851f6a11a02987c4d764
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2022-10-29 15:32:17 +0530

    refactoring

--- PingNet.py
+++ PingNet.py
@@ -8,25 +8,42 @@ import time
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
 import ping3
-i = 0
-n = 0
+
+sum_of_response_times = 0
+num_of_icmp_pkts = 0
+num_of_responses_recived = 1
+
 # Create an instance of tkinter frame or window
 win=Tk()
 
 def ping():
-    global i
-    global n
+    global sum_of_response_times
+    global num_of_icmp_pkts
+    global num_of_responses_recived
+
     t = ping3.ping("google.com")
-    if(t == None):t = 0
-    i = i+t
-    n+=1
-    print("Response time: ",t,"Avg Response time: ", (i/n))
-    if(t == 0 or (i/n) > 0.8 ):
+    tl = 0
+    if(t == None):
+        tl = 0
+    else:
+        tl = t
+        
+    sum_of_response_times = sum_of_response_times+tl
+    num_of_icmp_pkts+=1
+    if(t != 0):
+        num_of_responses_recived+=1
+    
+    avg_response_time = sum_of_response_times / num_of_responses_recived
+    
+    print("Response time: ",t,"Avg Response time: ", avg_response_time)
+
+    if(t == None or avg_response_time > 0.8 ):
         label.config(text="Net is Slow", fg="white", bg="red")
     elif(t == False):
         label.config(text="Connection is closed", fg="white", bg="blue")
     else:
         label.config(text="Net OK", fg="white", bg="green")
+    
     win.after(1000,ping)
 
 # Set the size of the window
@@ -38,5 +55,3 @@ label.pack()
 ping()
     
 win.mainloop()
-
-
