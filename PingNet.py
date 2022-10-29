dc48d72e80a2a8bb7b1021e09743995c6dbca633
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2022-10-29 12:05:41 +0530

    few enhancements

--- PingNet.py
+++ PingNet.py
@@ -8,22 +8,29 @@ import time
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
 import ping3
-
+i = 0
+n = 0
 # Create an instance of tkinter frame or window
 win=Tk()
 
 def ping():
+    global i
+    global n
     t = ping3.ping("google.com")
-    if(t == None):
+    if(t == None):t = 0
+    i = i+t
+    n+=1
+    print("Response time: ",t,"Avg Response time: ", (i/n))
+    if(t == 0 or (i/n) > 0.8 ):
         label.config(text="Net is Slow", fg="white", bg="red")
     elif(t == False):
-        label.config(text="Net Connection is closed", fg="white", bg="blue")
+        label.config(text="Connection is closed", fg="white", bg="blue")
     else:
         label.config(text="Net OK", fg="white", bg="green")
-    win.after(100,ping)
+    win.after(1000,ping)
 
 # Set the size of the window
-win.geometry("100x100")
+win.geometry("80x20")
 
 label = Label(text="Hi", fg="white", bg="black")
 label.pack()
