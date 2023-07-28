b9c708211bcb722a54bebddec823e87b9b21defc
MaheshMadushan <madu140@gmail.com>
2023-07-28 12:05:16 +0530

    Update

--- PingNet.py
+++ PingNet.py
@@ -18,24 +18,24 @@ def ping():
 
     response_time = ping3.ping("google.com")
     response_time_in_int = 0
-    if(t == None):
+    if(response_time == None):
         response_time_in_int = 0
     else:
         response_time_in_int = response_time
         
-    sum_of_response_times = sum_of_response_times+response_time_in_int
+    sum_of_response_times = sum_of_response_times + response_time_in_int
     num_of_icmp_pkts+=1
-    if(t != 0):
+    if(response_time != 0):
         num_of_responses_recived+=1
     
     avg_response_time = sum_of_response_times / num_of_responses_recived
-    squared_sum += (tl - avg_response_time)**2
+    squared_sum += (response_time_in_int - avg_response_time)**2
     std = (squared_sum / num_of_responses_recived) ** 0.5
     print("Response time: ",response_time,"Avg Response time: ", avg_response_time, "std deviation: ",std)
 
-    if(t == None or avg_response_time > 0.8 ):
+    if(response_time == None or avg_response_time > 0.8 ):
         label.config(text="Net is Slow", fg="white", bg="red")
-    elif(t == False):
+    elif(response_time == False):
         label.config(text="Connection closed", fg="white", bg="blue")
     else:
         label.config(text="Net OK", fg="white", bg="green")
